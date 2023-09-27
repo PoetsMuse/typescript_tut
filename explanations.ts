1a625085aec8f923af86f83dc7bd15be9d035475
Kate Glushkova <weirdis1@gmail.com>
2023-09-27 19:48:48 +0300

    function signatures

--- explanations.ts
+++ explanations.ts
@@ -1,11 +1,3 @@
-const inputs = document.querySelectorAll('input');
-
-console.log(inputs);
-
-inputs.forEach(input => {
-    console.log(input);
-});
-
 //explicitly indicating the type for the func parameter. It won't let assigning anything but number
 const circ = (diameter: number) => {
     return diameter * Math.PI;
@@ -47,4 +39,58 @@ let number: any = 25;
 
 //destructuring object
 let someObj: {name: any, age: any};
-someObj = {name: 'yoshi', age: '25'}
+someObj = {name: 'yoshi', age: '25'};
+
+//functions
+let greet: Function;
+
+greet = () => {
+    console.log('hello');
+}
+
+//optional parameters, like c use ? / default value for d, that is also optional
+const add = (a: number, b: number, c?: number|string, d: number = 10) => {
+    console.log(a + b);
+}
+add(5, 10)
+
+//Creating the types to reuse for multiple cases
+type StringOrNum = string|number;
+type objWithName = {name: string, uid: StringOrNum};
+
+const logDetails = (uid: StringOrNum, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
+}
+const greet1 = (user: objWithName) => {
+    console.log(`${user.name} says hello`);
+}
+const greetAgain = (user: objWithName) => {
+    console.log(`${user.name} says hello again`);
+}
+
+
+//Function signatures
+//ex1
+let oneMoreGreet: (a: string, b: string) => void; //signature
+
+oneMoreGreet = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`);
+}
+
+//ex2
+let calc: (a: number, b: number, c: string) => number; // signature
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if (action === 'add'){
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
+}
+
+//ex3
+let ninjaLogDetails: (obj: {name: string, age: number}) => void;
+
+ninjaLogDetails = (ninja: {name: string, age: number}) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+}
