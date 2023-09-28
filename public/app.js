7454743d126af01c8213586a3635fa404866edd9
Kate Glushkova <weirdis1@gmail.com>
2023-09-28 20:41:55 +0300

    Starting project

--- public/app.js
+++ public/app.js
@@ -1,4 +1,27 @@
 "use strict";
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invoice1 = new Invoice('Mario', 'work on website', 250);
+const invoice2 = new Invoice('Luigi', 'work on website', 300);
+console.log(invoice1, invoice2);
 const anchor = document.querySelector('a');
 console.log(anchor.href);
-const form = document.querySelector('form');
+const form = document.querySelector('.new-item-form');
+//console.log(form.children);
+//inputs
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,44 @@
+class Invoice{
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this. details = d;
+        this.amount = a;
+    }
+    format(){
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+
+const invoice1 = new Invoice('Mario', 'work on website', 250);
+const invoice2 = new Invoice('Luigi', 'work on website', 300);
+
+console.log(invoice1, invoice2)
+
 const anchor = document.querySelector('a')!;
 
 console.log(anchor.href);
 
-const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+//console.log(form.children);
+
+//inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        toFrom.value,
+        details.value,
+        amount.valueAsNumber
+    );
+})
