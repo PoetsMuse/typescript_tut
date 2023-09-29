1373a804f58db8ad4f2a90433855a49322308c29
Kate Glushkova <weirdis1@gmail.com>
2023-09-29 21:17:03 +0300

    upd

--- public/app.js
+++ public/app.js
@@ -8,21 +8,11 @@ import { Payment } from './classes/Payment.js';
 // docs.push(docOne);
 // docs.push(docTwo);
 // console.log(docs)
-// const invoice1 = new Invoice('Mario', 'work on website', 250);
-// const invoice2 = new Invoice('Luigi', 'work on website', 300);
-// const invoice3 = new Invoice('Yoshi', 'relax', 400);
-// let invoices: Invoice[] = []; // you cant push anything else but objects derived from class Invoice
-// invoices.push(invoice1);
-// invoices.push(invoice2);
-// invoices.push(invoice3);
-// invoices.forEach(inv => {
-//     console.log(inv.client, inv.amount, inv.format());
-// });
+
 //////////////////////////////////////////////////
-// const anchor = document.querySelector('a')!;
-// console.log(anchor.href);
+
 const form = document.querySelector('.new-item-form');
-//console.log(form.children);
+
 //inputs
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
