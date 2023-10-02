0b1c37712ef162b8464df3550ca9428f2acaddd0
Kate Glushkova <weirdis1@gmail.com>
2023-10-02 10:19:10 +0300

    rendering html template

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
 // let docOne: HasFormatter;
 // let docTwo: HasFormatter;
@@ -8,16 +9,29 @@ import { Payment } from './classes/Payment.js';
 // docs.push(docOne);
 // docs.push(docTwo);
 // console.log(docs)
-
+// const invoice1 = new Invoice('Mario', 'work on website', 250);
+// const invoice2 = new Invoice('Luigi', 'work on website', 300);
+// const invoice3 = new Invoice('Yoshi', 'relax', 400);
+// let invoices: Invoice[] = []; // you cant push anything else but objects derived from class Invoice
+// invoices.push(invoice1);
+// invoices.push(invoice2);
+// invoices.push(invoice3);
+// invoices.forEach(inv => {
+//     console.log(inv.client, inv.amount, inv.format());
+// });
 //////////////////////////////////////////////////
-
+// const anchor = document.querySelector('a')!;
+// console.log(anchor.href);
 const form = document.querySelector('.new-item-form');
-
+//console.log(form.children);
 //inputs
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+//list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -27,5 +41,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
